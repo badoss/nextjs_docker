6645c4c08b246d5d86451d9ab3707d6e2ec9a5e9
boo boo <thongrit.th@inet.co.th>
2022-12-08 23:56:28 +0700

    update detail

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! Nextjs doceker tailwindcss
-    </h1>
+    <div className='flex  place-content-center h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
+      <div className='self-center '>
+      <p className="font-sans text-3xl font-bold  text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-purple-500">
+         Nextjs doceker tailwindcss <br />
+      </p>
+      <p className="font-sans text-1xl  text-transparent bg-clip-text bg-gradient-to-r from-[#ffff00] to-cyan-500 " >bad0s</p>
+      </div>
+      
+
+      
+    </div>
+
   )
 }
